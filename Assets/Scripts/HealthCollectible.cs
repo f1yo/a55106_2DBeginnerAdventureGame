5360ba130711ea316f29596298c91c70d2d719b9
fly <Benylaranjinha@gmail.com>
2025-03-09 17:37:03 +0000

    Check character health before destroying collectibles

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -12,12 +12,10 @@ public class HealthCollectible : MonoBehaviour
         PlayerController controller = other.GetComponent<PlayerController>();
 
 
-        if (controller != null)
+        if (controller != null && controller.health < controller.maxHealth)
         {
             controller.ChangeHealth(1);
             Destroy(gameObject);
         }
-
     }
-
 }
